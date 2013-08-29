c1b029c73d943c44a8c69ab53083b45a2b5aeb83
andy375867463 <zhixiang.sun@bric.jp>
2013-08-29 14:26:24 +0900

    #2 test to deal the Issue
    test of dealing with the Issue

--- sa-struts-test2/src/main/java/usermanager/action/IndexAction.java
+++ sa-struts-test2/src/main/java/usermanager/action/IndexAction.java
@@ -5,6 +5,7 @@ public class IndexAction {
     @Execute(validator = false)
     public String index() {
         //this a test of GitHub push
+        //test of dealing with the Issue
         return "index.jsp";
     }
 }
